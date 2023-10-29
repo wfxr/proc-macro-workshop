3ba31a76c5cb5096b472e0c25d06251d808cf7e8
Wenxuan Zhang <wenxuangm@gmail.com>
2023-10-29 21:36:27 +0800

    finish 5 & 6
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput};
+use syn::{parse_macro_input, AngleBracketedGenericArguments, Data, DeriveInput, GenericArgument, Path, TypePath};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -20,21 +20,52 @@ pub fn derive(input: TokenStream) -> TokenStream {
             .iter()
             .filter_map(|f| f.ident.as_ref().map(|name| (name, &f.ty)))
             .for_each(|(name, ty)| {
+                let (ty, is_option) = match ty {
+                    syn::Type::Path(TypePath { path: Path { segments, .. }, .. }) => {
+                        if segments.len() == 1 && segments[0].ident == "Option" {
+                            match &segments[0].arguments {
+                                syn::PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) =>
+                                    match &args[0] {
+                                        GenericArgument::Type(ty) => (ty, true),
+                                        _ => unreachable!(),
+                                    },
+                                _ => unreachable!(),
+                            }
+                        } else {
+                            (ty, false)
+                        }
+                    }
+                    _ => unreachable!(),
+                };
+
                 builder_fields.push(quote! {
                     #name: Option<#ty>,
                 });
+
+
                 builder_defaults.push(quote! {
                     #name: None,
                 });
+
                 builder_methods.push(quote! {
                     pub fn #name(&mut self, #name: #ty) -> &mut Self {
                         self.#name = Some(#name);
                         self
                     }
                 });
-                build_stmts.push(quote! {
-                    #name: self.#name.as_ref().ok_or_else(|| "#name is missing").cloned()?,
-                });
+
+                match is_option {
+                    true => {
+                        build_stmts.push(quote! {
+                            #name: self.#name.clone(),
+                        });
+                    }
+                    false => {
+                        build_stmts.push(quote! {
+                            #name: self.#name.as_ref().ok_or_else(|| "#name is missing").cloned()?,
+                        });
+                    }
+                }
             }),
         _ => panic!("Only structs are supported!"),
     };
