81e7771097fe5fe251be9f5a5a583f3e0e13c69d
Wenxuan Zhang <wenxuangm@gmail.com>
2023-10-30 22:12:47 +0800

    refactor
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,94 +1,105 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, AngleBracketedGenericArguments, Data, DeriveInput, GenericArgument, Path, TypePath};
+use syn::{
+    parse_macro_input,
+    AngleBracketedGenericArguments,
+    Data,
+    DataStruct,
+    DeriveInput,
+    Fields,
+    FieldsNamed,
+    GenericArgument,
+    Path,
+    PathArguments,
+    PathSegment,
+    Type,
+    TypePath,
+};
 
-#[proc_macro_derive(Builder)]
+fn inner_type(ty: &Type) -> Option<&Type> {
+    match ty {
+        Type::Path(TypePath { path: Path { segments, .. }, .. }) if segments.len() == 1 => match &segments[0] {
+            PathSegment { ident, arguments } if ident == "Option" => match arguments {
+                PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) if args.len() == 1 =>
+                    match &args[0] {
+                        GenericArgument::Type(inner_ty) => Some(inner_ty),
+                        _ => None,
+                    },
+                _ => None,
+            },
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let struct_name = input.ident;
+    let sname = input.ident;
 
-    let builder_name = format!("{}Builder", struct_name);
-    let builder_name = syn::Ident::new(&builder_name, struct_name.span());
+    let bname = format!("{}Builder", sname);
+    let bname = syn::Ident::new(&bname, sname.span());
 
-    let mut builder_fields = Vec::new();
-    let mut builder_defaults = Vec::new();
-    let mut builder_methods = Vec::new();
-    let mut build_stmts = Vec::new();
-    match input.data {
-        Data::Struct(s) => s
-            .fields
-            .iter()
-            .filter_map(|f| f.ident.as_ref().map(|name| (name, &f.ty)))
-            .for_each(|(name, ty)| {
-                let (ty, is_option) = match ty {
-                    syn::Type::Path(TypePath { path: Path { segments, .. }, .. }) => {
-                        if segments.len() == 1 && segments[0].ident == "Option" {
-                            match &segments[0].arguments {
-                                syn::PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) =>
-                                    match &args[0] {
-                                        GenericArgument::Type(ty) => (ty, true),
-                                        _ => unreachable!(),
-                                    },
-                                _ => unreachable!(),
-                            }
-                        } else {
-                            (ty, false)
-                        }
-                    }
-                    _ => unreachable!(),
-                };
-
-                builder_fields.push(quote! {
-                    #name: Option<#ty>,
-                });
-
-
-                builder_defaults.push(quote! {
-                    #name: None,
-                });
-
-                builder_methods.push(quote! {
-                    pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                        self.#name = Some(#name);
-                        self
-                    }
-                });
-
-                match is_option {
-                    true => {
-                        build_stmts.push(quote! {
-                            #name: self.#name.clone(),
-                        });
-                    }
-                    false => {
-                        build_stmts.push(quote! {
-                            #name: self.#name.as_ref().ok_or_else(|| "#name is missing").cloned()?,
-                        });
-                    }
-                }
-            }),
-        _ => panic!("Only structs are supported!"),
+    let fields = match input.data {
+        Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) => named,
+        _ => unreachable!(),
     };
 
+    let bidents = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if inner_type(&f.ty).is_some() {
+            quote! { #name: #ty, }
+        } else {
+            quote! { #name: std::option::Option<#ty>, }
+        }
+    });
+
+    let bempty = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! { #name: None, }
+    });
+
+    let bfields = fields.iter().map(|f| {
+        let name = &f.ident;
+        if inner_type(&f.ty).is_some() {
+            quote! { #name: self.#name.clone(), }
+        } else {
+            quote! { #name: self.#name.as_ref().ok_or_else(|| concat!(stringify!(#name), " is missing")).cloned()?, }
+        }
+    });
+
+    let bmethods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = inner_type(&f.ty).unwrap_or(&f.ty);
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
     let expanded = quote! {
-        pub struct #builder_name {
-            #(#builder_fields)*
+        pub struct #bname {
+            #(#bidents)*
         }
 
-        impl #struct_name {
-            pub fn builder() -> #builder_name {
-                #builder_name {
-                    #(#builder_defaults)*
+        impl #sname {
+            pub fn builder() -> #bname {
+                #bname {
+                    #(#bempty)*
                 }
             }
         }
 
-        impl #builder_name {
-            #(#builder_methods)*
+        impl #bname {
+            #(#bmethods)*
 
-            pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
-                Ok(#struct_name {
-                    #(#build_stmts)*
+            pub fn build(&mut self) -> Result<#sname, Box<dyn std::error::Error>> {
+                Ok(#sname {
+                    #(#bfields)*
                 })
             }
         }
